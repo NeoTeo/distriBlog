330cf20634018c5b19c9bfe223a7f308629575e2
Teo Sartori <m@sartori.dk>
2017-01-12 17:14:36 +0100

    Add title and text from file to post.

--- post.py
+++ post.py
@@ -10,6 +10,7 @@ parser.add_argument('--text', nargs='?', type=argparse.FileType('r'), help='A te
 parser.add_argument('--image', help='An image file.')
 parser.add_argument('--video', help='An video file.')
 parser.add_argument('--audio', help='An audio file.')
+parser.add_argument('--title', help='The title of the post.')
 parser.add_argument('--author', help='The author of the post.')
 parser.add_argument('--date', help='The date of the post. Defaults to current date.')
 parser.add_argument('--time', help='The time of the post. Defaults to current time.')
@@ -25,20 +26,23 @@ file_to_read = args.text
 #call(["ipfs", "id"])
 # get the last post by resolving the ipns 
 p = Popen(['ipfs', 'name', 'resolve', 'QmRzsihDMWML1dNPa51qwD2dacvZPfTrqsQt4pi1DWGJFP'], stdin=PIPE, stdout=PIPE, stderr=PIPE)
-#output = p.communicate(b"blabla")
 for line in p.stdout.readlines():
     prev_post_hash = ntpath.basename(line.strip())
 
 #print("prev_post is : ")
 #print(prev_post)
 
-#print(output[0])
 #print(ntpath.basename(output[0]).strip())
 #print(file_to_read.read())
 print('<html>')
 print('<body>')
-print('<h1>')
-print('first post<br>')
+post_title = args.title
+if post_title is not None:
+    print('<h1>' + post_title + '</h1>')
+if file_to_read is not None:
+    post_text = file_to_read.read()
+    print(post_text)
+
 print('</body>')
 print('<data-prev-post-hash=\"' + prev_post_hash.decode() + '\"/>')
 print('</html>')
