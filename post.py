6ff3091a7c31683f0282d2fcc99bd945c9b2c66e
Teo Sartori <m@sartori.dk>
2017-01-25 15:17:33 +0100

    Change media file args. Simplify ipfs add call and parsing.

--- post.py
+++ post.py
@@ -10,11 +10,11 @@ import os.path              # for checking files exist
 
 def parse_arguments():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--text_file', nargs='?', type=argparse.FileType('r'), help='Name of file containing post text.', default=sys.stdin)
+    parser.add_argument('--text_file', nargs='?', type=argparse.FileType('r'), help='Name of text file.', default=sys.stdin)
     parser.add_argument('--root_hash', help='The IPNS root hash.')
-    parser.add_argument('--image', help='An image file.')
-    parser.add_argument('--video', help='An video file.')
-    parser.add_argument('--audio', help='An audio file.')
+    parser.add_argument('--image_file', nargs='?', type=argparse.FileType('r'), help='Name of image file.', default=argparse.SUPPRESS)
+    parser.add_argument('--video_file', nargs='?', type=argparse.FileType('r'), help='Name of video file.', default=argparse.SUPPRESS)
+    parser.add_argument('--audio_file', nargs='?', type=argparse.FileType('r'), help='Name of audio file.', default=argparse.SUPPRESS)
     parser.add_argument('--title', help='The title of the post.')
     parser.add_argument('--author', help='The author of the post.')
     parser.add_argument('--date', help='The date of the post. Defaults to current date.')
@@ -48,24 +48,28 @@ def write_post(post_data, root_post_hash, out_filename):
 
     outfile.close()
 
-def add_to_IPFS(out_filename):
+def add_to_IPFS(filename):
     # Add the new post to the IPFS network
-    p = Popen(['ipfs', 'add', '-r', out_filename], stdin=PIPE, stdout=PIPE, stderr=PIPE)
+    p = Popen(['ipfs', 'add', '-q', filename], stdin=PIPE, stdout=PIPE, stderr=PIPE)
 
     # Compile regular expression to extract hash
-    regex = re.compile(b'^added (Qm[\w]{44})')
+    #regex = re.compile(b'^added (Qm[\w]{44})')
     for line in p.stdout.readlines():
-        matches = regex.search(line)
-        post_hash = matches.group(1)
-        print("adding resulted in", post_hash.decode())
+        #matches = regex.search(line)
+        #post_hash = matches.group(1)
+        post_hash = line.strip().decode()
+        print("adding resulted in", post_hash)
 
-    return post_hash.decode()
+    return post_hash
 
 def publish(post_hash):
     # Publish the post as the IPNS 
     if post_hash:
         p = Popen(['ipfs', 'name', 'publish', post_hash], stdin=PIPE, stdout=PIPE, stderr=PIPE)
         print("Published ",post_hash," to ipns ")
+        lines = p.stdout.readlines()
+        print(lines)
+             
         for line in p.stdout.readlines():
             print("output is ",line)
 
